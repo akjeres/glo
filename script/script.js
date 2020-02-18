ada3cb25d5cc147120910171d3e0025edd2cc1b9
akjeres <akjeres@gmail.com>
2020-02-18 19:14:57 +0200

    Day #1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,42 @@
 'use strict';
-const startButton = document.querySelector('.start-button');
-console.log(startButton);
+const startButton = document.querySelector('.start-button'),
+      firstScreen = document.querySelector('.first-screen'),
+      mainForm = document.querySelector('.main-form'),
+      formCalculate = document.querySelector('.form-calculate'),
+      endButton = document.querySelector('.end-button'),
+      total = document.querySelector('.total'),
+      fastRange = document.querySelector('.fast-range');
+
+function showElement(elem) {
+    elem.style.display = '';
+}
+function hideElement(elem) {
+    elem.style.display = 'none';
+}
+function handlerCallBackForm(event) {
+    const target = event.target;
+    
+    if (target.classList.contains('want-faster')) {
+        target.checked ? showElement(fastRange) : hideElement(fastRange);
+    }
+    
+}
+
+
+startButton.addEventListener('click', function(event) {
+    showElement(mainForm);
+    hideElement(firstScreen);
+});
+
+endButton.addEventListener('click', function(event) {
+    for (const elem of formCalculate.elements) {
+        if (elem.tagName.toLowerCase() === 'fieldset') {
+            hideElement(elem);
+        }
+    }
+
+    showElement(total);
+
+});
+
+formCalculate.addEventListener('change', handlerCallBackForm);
